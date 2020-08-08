12f3eb1cd1773812243cc33d98efdd74bb4611cb
isiagi <isiagigeofrey0@gmail.com>
2020-08-08 16:56:51 +0300

    ft(made changes)

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.get("/", (req, res)=> {
     res.send("Up and Running")
 })
 
-app.listen(3000, ()=>{
+const Port = process.env.PORT || 3000
+
+app.listen(Port, ()=>{
     console.log('Server Up and runing')
 });
