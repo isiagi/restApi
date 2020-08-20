e2fbac1d8cc63e08315f901fea2351b280a7bc40
isiagi <isiagigeofrey0@gmail.com>
2020-08-20 12:13:05 +0300

    refactor

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 //Import Routes
-const authRoute = require('./routes/auth');
-const postRoute = require('./routes/post')
-
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
 
 dotenv.config();
 
 //connect to db
-mongoose.connect(process.env.DB_CONNECT
-,{ useUnifiedTopology: true, useNewUrlParser: true} ,
-()=> {
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
     console.log("connected to db");
-})
-
+  }
+);
 
 //middleware
-app.options('*', cors())
-app.use(express.json())
+app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, authorization, auth-token"
+  );
+  next();
+});
 
 //use Middleware
 
-app.use('/api/user', authRoute);
-app.use('/api/post', postRoute);
+app.use("/api/user", authRoute);
+app.use("/api/post", postRoute);
 
-app.get("/", (req, res)=> {
-    res.send("Up and Running")
-})
+app.get("/", (req, res) => {
+  res.send("Up and Running");
+});
 
-const Port = process.env.PORT || 3000
+const Port = process.env.PORT || 3000;
 
-app.listen(Port, ()=>{
-    console.log(`Server Up and runing ${Port}`)
+app.listen(Port, () => {
+  console.log(`Server Up and runing ${Port}`);
 });
